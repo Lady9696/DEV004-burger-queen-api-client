8c8c641618da19624b2f89cc029e9e54da7e486e
Leidy Suarez Miranda <leidysuarez55@gmail.com>
2023-06-06 13:02:23 -0500

    todo okay

--- BurguerQueenn/src/App.jsx
+++ BurguerQueenn/src/App.jsx
@@ -1,13 +1,39 @@
+import "./index.css";
+
+import { useState } from "react";
 
 
 
+function Login() {
 
-function App() {
-  return (
-    <div>buuuuuuuuuuuuuuuuuuuuu</div>
+  function eventLogin (e) {
+    e.preventDefault();
 
-  );
   
+  
+  
+  }
+  
+  // Aquì hago el formulario
+  return (
+    <>
+    
+    
+    <div className="squareLogin">
+   
+      <input type="text" className="emailInput" placeholder="Type your email" />
+      <input type="text" placeholder="Password" />
+      <button className="btnLogin" onClick={evento}> Login </button>
+
+    
+    
+    </div>
+    </>
+
+      
+  );
+
 }
 
-export default App
+
+export default Login
