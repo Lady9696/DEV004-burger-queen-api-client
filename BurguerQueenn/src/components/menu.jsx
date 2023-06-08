984f0c6a1a51fe226c80253755529e835dc33ed7
Leidy Suarez Miranda <leidysuarez55@gmail.com>
2023-06-08 18:51:16 -0500

    haciendo menu primera parte

--- BurguerQueenn/src/components/menu.jsx
+++ BurguerQueenn/src/components/menu.jsx
@@ -1,4 +1,45 @@
 
-export const Menu =() => {
+export const Menu = () => {
 
-}
+    return(
+      <>
+      <div className="squareMenuBreakfast">
+        <div className="containerAmericanCofee">
+        
+        </div>
+        <div className="containersandwich"></div>
+       
+        <div className="containerJuice"></div>
+        <img className="juice" src="https://i.ibb.co/4tGvFLT/juice.png" alt="juice"></img>
+        <div className="containerCapuccino"></div>
+        <img className="capuccino" src="https://i.ibb.co/2j30wj4/capuccino.png" alt="capuccino"></img>
+      </div>
+      <div className="squareMenuLunch">
+      <div className="containerburguer">
+      <img className="burguer" src="https://i.ibb.co/s3Cmk7p/burger1.png" alt="burguer"></img>
+      </div>
+        <div className="containerFranceFries">
+        <img className="franceFries" src="https://i.ibb.co/YBSThnw/frenchfries.png" alt="franceFries"></img>
+        </div>
+        <div className="containerOnionrings">
+        <img className="onionRings" src="https://i.ibb.co/J2gPkmG/onion.png" alt="onionRings"></img>
+        </div>
+        <div className="ContainerDoubleburguer"></div>
+        <img className="doubleBurguer" src="https://i.ibb.co/2ZyjHvB/double-Burguer.png" alt="doubleBurguer"></img>
+        
+        <div className="containerWaterBig"></div>
+        <img className="bigWater" src="https://i.ibb.co/qBv0WV4/5213596.png" alt="water700ml"></img>
+        <div className="containerWater500ml"></div>
+        <img className="smallWater" src="https://i.ibb.co/9HZdHNb/water.png" alt="water500ml"></img>
+        <div className="containerCoke"></div>
+        <img className="coke" src="https://i.ibb.co/kcP8kLB/07-01.png" alt="coke500ml"></img>
+        <div className="containerBeer"></div>
+        <img className="coke" src="https://i.ibb.co/NjhWL3h/beer.png" alt="beer"></img>
+        </div>
+      
+      
+      
+      
+      </>  
+    );
+    }
