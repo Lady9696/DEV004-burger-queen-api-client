73686bf37d7b4502a91cdb070c6dd0c2e01e2cac
Leidy Suarez Miranda <leidysuarez55@gmail.com>
2023-06-07 19:07:05 -0500

    login funciona

--- BurguerQueenn/src/App.jsx
+++ BurguerQueenn/src/App.jsx
@@ -1,66 +1,78 @@
 import "./index.css";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 // useState me permite saber el estado del usuario, si esta logueado
-//o no
+//import axios
+
+
+import axios from 'axios';
 
 
 
 
 function Login() {
-  
- const [milogin, setlogin] = useState(false);
- //para capturar le nombre del usuario y la clave
+
+  //const [login, setlogin] = useState("false");
+  //para capturar le nombre del usuario y la clave con el evento onchage
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const getValues = (e) =>{
-    
-  e.preventDefault();
-  const email = e.target.email.value;
-  const password= e.target.password.value; 
-  console.log(email);
-  console.log(password);
-  
+  const getValues = (e) => {
+    e.preventDefault();
 
-  }
-  
-  
-  // Aquì hago el formulario
+    
+    const objeto = {
+      email:e.target.email.value,
+      password:e.target.password.value
+
+    }
+    
+    //axios
+    //se utiliza  axios, con el mètodo post
+    axios.post("http://localhost:8080/login", objeto)
+    .then((response ) => {
+      console.log(response, 'hola')
+
+    })
+    .catch ((error) => {
+      console.log(error.response.data)
+
+
+    })
+
+  } // Aquì hago el formulario
   return (
     <>
- 
-    <div className="squareLogin">
-     
-     
-  <div className="conatinerTitleImage">
-    <h1>Burguer Queen </h1>
-    <img className="logo"src="https://i.ibb.co/7SRPXzW/burguer.png" alt="burguer" ></img>
-    </div>    
-    
-     
-    <form className="loginForm"onSubmit = { getValues }>
-      <label className= "emailLabel">
-        <input type ="text" name="email" className="inputEmail" placeholder="Type your email"   required>
-        </input>
-      </label>
-      <label className="passworrLabel">
-        <input className="inputPassword" name="password" placeholder="Password"  required></input>
-      </label>
-      <button type = "submit" className="btnLogin"> Login</button>
 
+      <div className="squareLogin">
+        <div className="conatinerTitleImage">
+          <h1>Burguer Queen </h1>
+          <img className="logo" src="https://i.ibb.co/7SRPXzW/burguer.png" alt="burguer" ></img>
+        </div>
+        <form className="loginForm" onSubmit={getValues}>
+          <label className="emailLabel">
+            <input type="text" name="email" className="inputEmail" placeholder="Type your email" onChange={(e) => setEmail(e.target.value)} required>
+            </input>
+          </label>
+          <label className="passworrLabel">
+            <input className="inputPassword" name="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} required></input>
+          </label>
+          <button type="submit" className="btnLogin"> Login</button>
+        </form>
 
+        <div className="footerLogin"></div>
 
-
-
-    </form>
-      
-    <div className="footerLogin"></div> 
-    
-    </div>
-    </>     
+      </div>
+    </>
   );
 
 }
 
 
 export default Login
+/*axios.post("http://localhost:8080/")
+  //esto me devuelve una promesa
+  .then((response)=>{
+    console.log(response);
+
+  })*/
