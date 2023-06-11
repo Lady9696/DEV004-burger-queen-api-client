bcb52ecffaf5401642f1cfe4c9c26c5145937bdc
Leidy Suarez Miranda <leidysuarez55@gmail.com>
2023-06-11 16:36:47 -0500

    obtengo los prodcutos al igual que el token

--- BurguerQueenn/src/components/contex/dataContex.jsx
+++ BurguerQueenn/src/components/contex/dataContex.jsx
@@ -5,56 +5,37 @@ import PropTypes from "prop-types";
 
 import { dataContex } from "./eso"
 
+const access = window.localStorage.getItem("accessToken");
+const user = JSON.parse(window.localStorage.getItem("user"));
+
+// Verifica si el token y los datos del usuario existen en el localStorage
+if (access && user) {
+  // Agrega el token y los datos del usuario como encabezados personalizados
+  axios.defaults.headers.common["Authorization"] = `Bearer ${access}`;
+  axios.defaults.headers.common["User-Data"] = JSON.stringify(user);
+}
+
 const DataProvider = ({children}) => {
 const [data, setData] = useState([]);
+useEffect(() => {
 
-useEffect(()=> {
-    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFuaXRhLmJvcmdAc3lzdGVycy54eXoiLCJpYXQiOjE2ODY1MTI0OTUsImV4cCI6MTY4NjUxNjA5NSwic3ViIjoiMSJ9.lEKRZ-Q-B4ch9X8pHX7f6yXVPDSACw3LZhEy53_oK4E";
 
-    // Obtén los datos
-    const userData = {
-      email: "anita.borg@systers.xyz",
-      id: 1,
-      role: "admin"
-    };
-    // Agrega el token con los datos de usuario como encabezado de autorización
-    axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-    axios.defaults.headers.common["User-Data"] = JSON.stringify(userData);
     axios.get("http://localhost:8080/products")
-    .then((response) => {
-        // Procesar la respuesta exitosa
+      .then((response) => {
         setData(response.data);
       })
       .catch((error) => {
-        // Manejar el error
         console.error("Error en la solicitud:", error);
-      }); 
-},[]);
+      });
+  }, []);
 
-
-return <dataContex.Provider value={{data}} >{children}</dataContex.Provider>;
- 
-
+  return (
+    <dataContex.Provider value={{ data }}>{children}</dataContex.Provider>
+  );
 };
 
 DataProvider.propTypes = {
-    children: PropTypes.node.isRequired
-  };
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  children: PropTypes.node.isRequired,
+};
 
 export default DataProvider;
