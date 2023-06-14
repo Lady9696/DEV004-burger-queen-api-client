35b71bd5f207e38ad3d53096a33eb43a3886910f
Leidy Suarez Miranda <leidysuarez55@gmail.com>
2023-06-13 19:33:29 -0500

    estudiar context

--- BurguerQueenn/src/components/contex/dataContex.jsx
+++ BurguerQueenn/src/components/contex/dataContex.jsx
@@ -1,36 +1,45 @@
-import {  useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import axios from "axios";
 import PropTypes from "prop-types";
 
 
 import { dataContex } from "./eso"
 
-const access = window.localStorage.getItem("accessToken");
-const user = JSON.parse(window.localStorage.getItem("user"));
 
 // Verifica si el token y los datos del usuario existen en el localStorage
-if (access && user) {
-  // Agrega el token y los datos del usuario como encabezados personalizados
-  axios.defaults.headers.common["Authorization"] = `Bearer ${access}`;
-  axios.defaults.headers.common["User-Data"] = JSON.stringify(user);
-}
 
-const DataProvider = ({children}) => {
-const [data, setData] = useState([]);
-useEffect(() => {
+
+const DataProvider = ({ children }) => {
+  const [products, setProducts] = useState([]);
+  const [access, setAccess] = useState(window.localStorage.getItem("accessToken"));
+  const [user, setUser] = useState(JSON.parse(window.localStorage.getItem("user")));
+
+  const value = useMemo(()=>{
+    products, setAccess, setUser, access, user, setProducts
+  },[access, products, user])
+  useEffect(() => {
 
 
     axios.get("http://localhost:8080/products")
       .then((response) => {
-        setData(response.data);
+        setProducts(response.data);
       })
       .catch((error) => {
         console.error("Error en la solicitud:", error);
       });
   }, []);
+  useEffect(()=>{
+    if(user && access ){
+      axios.defaults.headers.common["Authorization"] = `Bearer ${access}`;
+      axios.defaults.headers.common["User-Data"] = JSON.stringify(user);
+    }
+
+  },[user, access]);
+  
+
 
   return (
-    <dataContex.Provider value={{ data }}>{children}</dataContex.Provider>
+    <dataContex.Provider value={ value }>{children}</dataContex.Provider>
   );
 };
 
--- BurguerQueenn/src/components/login/App.jsx
+++ BurguerQueenn/src/components/login/App.jsx
@@ -2,27 +2,27 @@ import "../login/login.css";
 //import { useState } from "react";
 import { useNavigate } from "react-router-dom"
 import axios from 'axios';
-
+/* se crea el componente Login */
 function Login() {
-
-  const navigate = useNavigate();
-
+  /*Se declara navigate para poder utilizar el hook useNavigate
+*/  const navigate = useNavigate();
+/*se crea una funcion y dentro de ella un evento que me permite capturar los valores del input email y contraseña*/
   const getValues = (e) => {
     e.preventDefault();
+    /*se crea un objeto con el value de los inputs*/
     const objeto = {
       email: e.target.email.value,
       password: e.target.password.value
     }
-
-
-    //se utiliza  axios, con el mètodo post
+   /*se realiza la peticion con post y se le pasa el objeto con los values de los inputs*/
     axios.post("http://localhost:8080/login", objeto)
+    /*si el usuario ingresa el correo y contraseña correctos, se va dirigir al menu*/
       .then((response) => {
         console.log(response, 'hola')
         window.localStorage.setItem("accessToken", response.data.accessToken);
         window.localStorage.setItem("user", JSON.stringify(response.data.user));
         navigate("/menu");
-      })
+      })/*de lo contario, la promesa es rechazada y permanecene login*/
       .catch((error => {
         if(error.response.data === "Incorrect Password"){
           alert("incorrect Pasword");
