df17c14a17d034dccdd37b3cd1763e63c0e2b81a
Leidy Suarez Miranda <leidysuarez55@gmail.com>
2023-06-15 17:59:07 -0500

    filtros realizados

--- BurguerQueenn/src/components/menu/menu.jsx
+++ BurguerQueenn/src/components/menu/menu.jsx
@@ -2,44 +2,74 @@
 //import axiosBD from "../axios";
 
 import "../menu/menu.css"
-import { Navigate } from "react-router-dom";
+//import { Navigate } from "react-router-dom";
 
 import { useContext, useState } from "react";
 import { dataContex } from "../contex/eso";
 
 export const Menu = () => {
   const { products } = useContext(dataContex);
-  const [filtro, setFiltro] = useState("");
-  const [productosFiltrados, setProductosFiltrados] = useState([]);
+  const [filterType, setFilterType] = useState(null);
+  //const [breakfastFilter, setBreakfastFilter] = useState(null)
   /*
   access, user
   if (access === null && user === null) {
     return <Navigate to="/" replace={true} />
   }
 */
-  const handleFiltro = () => {
-    const desayunoFiltrado = products.filter((product) => product.type === "Desayuno");
-    setFiltro("Desayuno");
-    setProductosFiltrados(desayunoFiltrado);
+  const handleFiltro = (type) => {
+    setFilterType(type);
+ 
+
   };
-  //const[productos,setProductos] = useState();
-  return (
-    <div className="squareMenu">
-      <button className="buttonbreakfast"onClick={handleFiltro}></button> 
-      {productosFiltrados.map((product) => {
-            
-        return (
-          <div key={product.id} className="containerCard">
-            <form className="formmenu">
-              <div className="containerimageName">
-                <h2 className="productName">{product.name}</h2>
-                <img className="productImage" src={product.image} alt="burguer" />
-                <h2>{product.price}</h2>
-              </div>
-            </form>
+  const filteredProducts = filterType
+    ? products.filter((product) => product.type === filterType)
+    : products;
+  
+  
+  return (   <div className="squareMenu">
+    <button className="buttonLunch" onClick={() => handleFiltro("Desayuno")}>Desayuno</button>
+    <button className="buttonFilter" onClick={() => handleFiltro("Almuerzo")}>Almuerzo</button>
+    {filteredProducts.map((product) => (
+      <div key={product.id} className="containerCard">
+        <form className="formmenu">
+          <div className="containerimageName">
+            <h2 className="productName">{product.name}</h2>
+            <img className="productImage" src={product.image} alt="burger" />
+            <h2>{product.price}</h2>
           </div>
-        );
-      })}
-    </div>
+        </form>
+      </div>
+    ))}
+  </div>
+    
+  
   );
-}
+};
+  
+
+/*{lunchFilter === null ? (
+      products.map((product) => (
+        <div key={product.id} className="containerCard">
+          <form className="formmenu">
+            <div className="containerimageName">
+              <h2 className="productName">{product.name}</h2>
+              <img className="productImage" src={product.image} alt="burger" />
+              <h2>{product.price}</h2>
+            </div>
+          </form>
+        </div>
+      ))
+    ) : (
+      lunchFilter.map((product) => (
+        <div key={product.id} className="containerCard">
+          <form className="formmenu">
+            <div className="containerimageName">
+              <h2 className="productName">{product.name}</h2>
+              <img className="productImage" src={product.image} alt="burger" />
+              <h2>{product.price}</h2>
+            </div>
+          </form>
+        </div>
+      ))
+    )}*/
