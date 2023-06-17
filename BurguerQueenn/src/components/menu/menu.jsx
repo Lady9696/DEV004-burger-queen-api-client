301bdf1ecb1cdc047e2b7696dfa78f253694eb81
Leidy Suarez Miranda <leidysuarez55@gmail.com>
2023-06-17 18:30:28 -0500

    cada producto se contabilia de mnaera indepedniente

--- BurguerQueenn/src/components/menu/menu.jsx
+++ BurguerQueenn/src/components/menu/menu.jsx
@@ -27,20 +27,30 @@ export const Menu = () => {
   const filteredProducts = filterType ? products.filter((product) => product.type === filterType): products;
   //Aqui voy a agregarle el onclick a las imagenes
 
-  const AddToCart = () => {
+  const AddToCart = (product, count) => {
+    
     setCart([...cart, product])
-    setCount(count+1)
-    console.log('producto agg'+ product)
+    setCount((prevCount) => {
+      const updatedCount = { ...prevCount };
+      updatedCount[product.id] = (updatedCount[product.id] || 0) + 1;
+      return updatedCount;
+    });
+  
+    console.log(product, count);
   }
   
-  const removeFromCart = () => {
-    if (count > 0) {
-      const updatedCart = cart.slice(0, -1);
-      setCart(updatedCart);
-      setCount(count - 1);
-      
-    }
+  
+  const removeFromCart = (product) => {
+    
+    
   }
+  
+  /*
+  if (count > 0) {
+    setCart(updatedCart);
+    console.log('producto eliminado'+ product)
+    const updatedCart = cart.filter((item) => item.id !== product.id);
+  */
   
   return (
     <div className="squareMenu">
@@ -51,18 +61,19 @@ export const Menu = () => {
           <p>Producto</p>
           {filteredProducts.map((product) => (
             <div key={product.id} className="containerCard">
-              <div className="productName">{product.name}</div>
+              <p>{product.name}</p>
               <img className="productImage" src={product.image} alt="burger" />
-              <div className="productPrice">{product.price}</div>
+              <p>{product.price}</p>
               
               <div className="buttonGroup">
                 <div className="Container-increase">
                   
-                  <ion-icon name="add-outline"></ion-icon>
+                  <ion-icon name="add-outline" onClick={()=>AddToCart(product, count)}></ion-icon>
                 </div>
+                <p> <p>{count[product.id] || 0}</p></p>
                 <div className="Container-decrease">
                   
-                  <ion-icon name="remove-outline"></ion-icon>
+                  <ion-icon name="remove-outline" onClick={() => removeFromCart(product)}></ion-icon>
                 </div>
               </div>
             </div>
--- BurguerQueenn/src/components/services/useProducts.js
+++ BurguerQueenn/src/components/services/useProducts.js
@@ -6,21 +6,21 @@ import axios from "axios";
 
 export const useProducts = () =>{
 
-    const [status, setStatus] = useState('idle');
-    const { products, setProducts } = useContext(dataContex);
-    useEffect(() => {
-       setStatus("pending")
-        axios
-          .get("http://localhost:8080/products")
-          .then((response) => {
-            console.log( response );
-            //cuando se hace la solicitud, se guarda en la variable products
-            setProducts(response.data);
-            setStatus("sucess")
-          })
-          .catch((error) => {
-            setStatus("error")
-          });
-      }, []);
-    return {productsPromiseStatus: status,products}
+  const [status, setStatus] = useState('idle');
+  const { products, setProducts } = useContext(dataContex);
+  useEffect(() => {
+    setStatus("pending")
+    axios
+      .get("http://localhost:8080/products")
+      .then((response) => {
+        console.log( response );
+        //cuando se hace la solicitud, se guarda en la variable products
+        setProducts(response.data);
+        setStatus("sucess")
+      })
+      .catch((error) => {
+        setStatus("error")
+      });
+  }, []);
+  return {productsPromiseStatus: status,products}
 }
