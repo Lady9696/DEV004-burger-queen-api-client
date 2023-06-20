94ebb9d89a4bbc35117dcb0731cf8d03c34781a4
Leidy Suarez Miranda <leidysuarez55@gmail.com>
2023-06-20 15:37:38 -0500

    haciendo la historia 4

--- BurguerQueenn/src/components/waiters/waiter.jsx
+++ BurguerQueenn/src/components/waiters/waiter.jsx
@@ -1,17 +1,41 @@
+
 import "../waiters/waiter.css"
 import axios from "axios" 
+import { Navigate, Link } from "react-router-dom";
+import {  useContext, useState } from "react";
+import { dataContex } from "../contex/eso";
 
 export const Waiter = () => {
-  //para ver los pedidos debo hacer una peticiòn get de las ordenes y filtrar por status:'done'
+  const [orders, setOrders] = useState([]);
+
+  const { access, user} = useContext(dataContex);
+  if (access === null && user === null) {
+    return <Navigate to="/" replace={true} />
+  }
+
+
+  //para ver los pedidos debo hacer una peticiòn get de las ordenes 
   axios.get("http://localhost:8080/orders")
     .then((response) => {
-      console.log(response, 'me traigo las ordenes');
- 
+        //las ordenes sin filtro
+        console.log(response.data)
+        const orders = response.data; 
+      //hago un fitlro para traerme solo las que tiene status_done
+      const filteredOrders = orders.filter(order => order.status === 'done');
+      //console.log(filteredOrders);
+      
+
     })
     .catch((error) =>{
       console.log(error);
 
     });
     
+  return (
+    <div className="squareWaiter">
+      
+    </div>
+        
+  );
 
 }
